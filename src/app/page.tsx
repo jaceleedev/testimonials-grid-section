ed7198aa425ac5577bc0169c2365c14e339ea583
jaceleedev <jaceleedev@gmail.com>
2024-07-26 16:57:10 +0900

    feat: :iphone: implement fully responsive grid layout for testimonials section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,6 +12,7 @@ interface TestimonialData {
   showQuote?: boolean;
   reducedPadding?: boolean;
   imageBorder?: string;
+  isFirstCard?: boolean;
 }
 
 const testimonialData: TestimonialData[] = [
@@ -25,7 +26,7 @@ const testimonialData: TestimonialData[] = [
       '“ I was an EMT for many years before I joined the bootcamp. I’ve been looking to make a transition and have heard some people who had an amazing experience here. I signed up for the free intro course and found it incredibly fun! I enrolled shortly thereafter. The next 12 weeks was the  best - and most grueling - time of my life. Since completing the course, I’ve  successfully switched careers, working as a Software Engineer at a VR startup. ”',
     textColor: 'text-white',
     backgroundColor: 'bg-moderate-violet',
-    gridArea: 'col-span-2',
+    gridArea: 'col-span-2 max-sm:col-span-1',
     showQuote: true,
     imageBorder: 'border-2 border-solid border-[#A775F1]',
   },
@@ -49,7 +50,7 @@ const testimonialData: TestimonialData[] = [
       '“ Thank you for the wonderful experience! I now have a job I really enjoy, and make a good living while doing something I love. ”',
     textColor: 'text-dark-grayish-blue',
     backgroundColor: 'bg-white',
-    gridArea: 'col-span-1 row-start-2',
+    gridArea: 'col-span-1 row-start-2 max-sm:row-start-auto',
     reducedPadding: true,
   },
   {
@@ -62,7 +63,7 @@ const testimonialData: TestimonialData[] = [
       '“ The staff seem genuinely concerned about my progress which I find really refreshing. The program gave me the confidence necessary to be able to go out in the world and present myself as a capable junior developer. The standard is above the rest. You will get the personal attention you need from an incredible community of smart and amazing people.  ”',
     textColor: 'text-white',
     backgroundColor: 'bg-dark-blackish-blue',
-    gridArea: 'col-span-2 row-start-2',
+    gridArea: 'col-span-2 row-start-2 max-sm:col-span-1 max-sm:row-start-auto',
     reducedPadding: true,
     imageBorder: 'border-2 border-solid border-[#733FC8]',
   },
@@ -75,20 +76,22 @@ const testimonialData: TestimonialData[] = [
       '“ Before joining the bootcamp, I’ve never written a line of code. I needed some structure from professionals who can help me learn programming step by step. I was encouraged to enroll by a former student of theirs who can only say wonderful things about the program. The entire curriculum and staff did not disappoint. They were very hands-on and I never had to wait long for assistance. The agile team project, in particular, was outstanding. It took my learning to the next level in a way that no tutorial could ever have. In fact, I’ve often referred to it during interviews as an example of my developent experience. It certainly helped me land a job as a full-stack developer after receiving multiple offers. 100% recommend!  ”',
     textColor: 'text-dark-grayish-blue',
     backgroundColor: 'bg-white',
-    gridArea: 'col-start-4 row-span-2',
+    gridArea:
+      'col-span-1 row-span-2 max-lg:col-span-3 max-lg:row-span-1 max-sm:col-span-1',
     reducedPadding: true,
   },
 ];
 
 export default function Home() {
   return (
-    <main className="flex justify-center items-center w-screen h-screen bg-light-grayish-blue">
-      <section className="grid grid-cols-4 grid-rows-[auto_auto] gap-x-[30px] gap-y-6 max-w-[1110px]">
+    <main className="flex justify-center items-center w-screen min-h-screen bg-light-grayish-blue max-lg:py-[71px] max-lg:px-6">
+      <section className="grid grid-cols-4 grid-rows-[auto] gap-x-[30px] gap-y-6 max-w-[1110px] max-lg:grid-cols-3 max-sm:grid-cols-1">
         {testimonialData.map((testimonial, index) => (
           <Testimonial
             key={testimonial.name}
             {...testimonial}
             isFirstRow={index < 2}
+            isFirstCard={index === 0}
           />
         ))}
       </section>
