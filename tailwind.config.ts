2f56f317b9f28146b93332c83346229e42fbf40f
jaceleedev <jaceleedev@gmail.com>
2024-07-26 11:51:50 +0900

    feat: :wrench: implement design system configurations

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,22 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        'moderate-violet': '#733FC8',
+        'dark-grayish-blue': '#48556A',
+        'dark-blackish-blue': '#19202D',
+        'light-gray': '#CFCFCF',
+        'light-grayish-blue': '#ECF2F8',
+      },
+      fontFamily: {
+        barlowSemiCondensed: ['var(--font-barlow-semi-condensed)'],
       },
     },
   },
